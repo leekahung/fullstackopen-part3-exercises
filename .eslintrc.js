b4ec265fe2d024377966117e345d2c7ea902f574
Ka Hung Lee <lee.kahung414@gmail.com>
2023-01-13 19:37:44 -0800

    Exercise 3.22 - Added linting from eslint

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    eqeqeq: "error",
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "no-console": 0,
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
